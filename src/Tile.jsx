59b86598a9e3e89ba4a4d76eb23931a6563d9405
Lebene Gbebleou-Sleem <128799752+LebeneSleem@users.noreply.github.com>
2024-02-26 22:23:53 +0000

    Update Tile.jsx

--- src/Tile.jsx
+++ src/Tile.jsx
@@ -3,31 +3,32 @@ export function Tile({ content: Content, flip, state }) {
     case "start":
       return (
         <Back
-          className="inline-block h-8 w-8 bg-blue-300 text-center"
+          className="inline-block h-20 w-820 bg-blue-300 rounded-2xl"
           flip={flip}
         />
       );
     case "flipped":
       return (
-        <Front className="inline-block h-8 w-8 bg-green-500">
+        <Front className="inline-block h-20 w-20 rounded-2xl flex items-center justify-center bg-blue-600">
           <Content
             style={{
               display: "inline-block",
-              width: "100%",
-              height: "100%",
+              width: "80%",
+              height: "80%",
               verticalAlign: "top",
+              color: "#fcfcfc",
             }}
           />
         </Front>
       );
     case "matched":
       return (
-        <Matched className="inline-block h-8 w-8 text-gray-300">
+        <Matched className="inline-block h-20 w-20 rounded-2xl flex items-center justify-center text-gray-500">
           <Content
             style={{
               display: "inline-block",
-              width: "100%",
-              height: "100%",
+              width: "80%",
+              height: "80%",
               verticalAlign: "top",
             }}
           />
@@ -39,11 +40,7 @@ export function Tile({ content: Content, flip, state }) {
 }
 
 function Back({ className, flip }) {
-  return (
-    <div onClick={flip} className={className}>
-      ?
-    </div>
-  );
+  return <div onClick={flip} className={className}></div>;
 }
 
 function Front({ className, children }) {
